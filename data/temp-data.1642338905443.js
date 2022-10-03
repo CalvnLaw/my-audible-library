654a245e576940b12d6433a0f71e8380917fbbd7
CalvnLaw <96386649+CalvnLaw@users.noreply.github.com>
2022-10-03 08:17:04 -0400

    --

--- data/temp-data.1642338905443.js
+++
@@ -1,5 +0,0 @@
-<<<<<<< Updated upstream:data/temp-data.1646439401508.js
-window.tempDataJSON = {"books":true,"series":true,"collections":true,"wishlist":true,"extras":{"domain-extension":".com","pages":{},"subPageStates":[{"key":"categories","enabled":true},{"key":"authors","enabled":true},{"key":"publishers","enabled":true},{"key":"series","enabled":true},{"key":"narrators","enabled":true}],"cacheID":1646439401508}};
-=======
-window.tempDataJSON = {"books":true,"series":true,"collections":true,"wishlist":true,"extras":{"domain-extension":".com","pages":{},"subPageStates":[{"key":"categories","enabled":true},{"key":"authors","enabled":true},{"key":"publishers","enabled":true},{"key":"series","enabled":true},{"key":"narrators","enabled":true}],"cacheID":1642338905443}};
->>>>>>> Stashed changes:data/temp-data.1642338905443.js
